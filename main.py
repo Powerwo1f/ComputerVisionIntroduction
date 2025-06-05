efb54f02ca2c9abe4ffb0872186e57be4c97e46e
vozhd21 <alexey.belous@hygge.software>
2025-06-05 19:58:28 +0200

    Stopping training - callback introduction

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
-data = tf.keras.datasets.fashion_mnist
 
-(training_images, training_labels), (test_images, test_labels) = data.load_data()
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy') > 0.95):
+            print("\nReached 95% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+callbacks = myCallback()
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
 # Normalizing increases performance (the best if the data is between 0 and 1, or so)
 training_images = training_images / 255.0
@@ -17,7 +25,7 @@ model.compile(optimizer = 'adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=50, callbacks=[callbacks])
 
 model.evaluate(test_images, test_labels)
 
